acfe830b3fddb8e122faa13bba63bef64aec50f8
Jay Zweedyk Hamady <jay@premiummedia360.com>
2019-01-18 10:20:08 -0500

    First Upload

---
+++ config.js
@@ -0,0 +1,14 @@
+
+var zipCode = '49503';
+var countryCode = 'US';
+var longitude = 42.9634;
+var latitude = -85.6681;
+var darkSkyID = '6b93954aff24c2935cc6722594559c6d';
+var cityName = "Grand Rapids";
+var imgDay = 'img/GrandRapids_day.png';
+var imgNight = 'img/GrandRapids_night.png';
+var APPID = '079b78918f4b98bab2cfaa210cf63f89';
+var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+var timeOffsetminutes = 15;
+var weatherRefreshMinutes = 1;
+var forecastNumberHoursSkip = 2;
