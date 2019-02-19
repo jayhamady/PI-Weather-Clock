234581ba44ec83b1d17409df56fbbe4041ca7df7
Jay Zweedyk Hamady <jay@premiummedia360.com>
2019-02-19 16:46:59 -0500

    Config Blank

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 
+// Set for Grand Rapids, MI for now
 var longitude = 42.9634;
 var latitude = -85.6681;
 //Sign up for a darksky account https://darksky.net/dev/register
-var darkSkyID = 'fbafffc24be575025775ade69b897a7d';
+var darkSkyID = '';
 //City to display 
 var cityName = "Grand Rapids";
 //Image for Day
@@ -14,8 +15,8 @@ var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 //Full day names
 var fullDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 //Time to offset clock time in minutes
-var timeOffsetminutes = 15;
+var timeOffsetminutes = 0;
 //Time to refresh weather info in minutes - note free account gets 1000 a day
-var weatherRefreshMinutes = 1;
+var weatherRefreshMinutes = 15;
 //Forecast number of hour to skip on each hour forecast
 var forecastNumberHoursSkip = 3;
